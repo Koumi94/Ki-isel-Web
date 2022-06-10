5b636b5ad5684dc33634303fbcfca60e20663ff5
Mamadou Aliou Barry <51833625+Koumi94@users.noreply.github.com>
2022-06-10 16:28:56 +0300

    setting database

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
-dbPassword = 'mongodb+srv://koumi:'+ encodeURIComponent('1234') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
-
-module.exports = {
-    mongoURI: dbPassword
-};
-
-
+// let dbPassword = 'mongodb+srv://koumi:'+ encodeURIComponent('1234') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 // module.exports = {
-//     MongoURI : 'mongodb+srv://koumi:1234@atlascluster.uymku.mongodb.net/?retryWrites=true&w=majority'
-// }
+//     mongoURI: dbPassword
+// };
+
+
+
+ module.exports = {
+     MongoURI : 'mongodb+srv://koumi:1234@atlascluster.uymku.mongodb.net/?retryWrites=true&w=majority'
+ }
